c7a79e959263fee400929164b1bf1008a9253c82
CharlieRoberts3D <30162653@students.lincoln.ac.uk>
2026-05-04 01:06:54 +0100

    Update veterinary_practice.py

--- CMP1139-assessment2-starter-code/veterinary_practice.py
+++ CMP1139-assessment2-starter-code/veterinary_practice.py
@@ -5,14 +5,19 @@ from medication import Medication
 from owner import Owner
 import utils
 
+
 class veterinary_practice:
-    """ Holds the data required for the Veterinary Practice application and allows:
+    """
+    Holds the data required for
+    the Veterinary Practice application and allows:
         - pets to be registered
         - appointments for pets to be created and attended
         - medications to be stocked
         - prescriptions to be created, prepared and collected.
 
-        Note, in a real systems, use data storage rather than a set of arrays and an MVC architecture.
+        Note, in a real systems,
+        use data storage rather than
+        a set of arrays and an MVC architecture.
     """
 
     def __init__(self):
@@ -23,9 +28,9 @@ class veterinary_practice:
         self.appointments = []
 
     def register_pet(self, pet_name, owner_name, species):
-        """ Registers a pet with the vets. 
+        """ Registers a pet with the vets.
             If the owner does not exist, a new owner is created.
-        
+
         :param self
         :param pet_name (string): The name of the pet
         :param owner_name (string): The name of a new or existing owner
@@ -39,7 +44,10 @@ class veterinary_practice:
         owner.add_pet(pet_name, species)
 
     def create_appointment(self, appointment):
-        """ Adds the appointment and returns the appointment ID (the ID is list index)"""
+        """
+        Adds the appointment and
+        returns the appointment ID (the ID is list index)
+        """
         self.appointments.append(appointment)
 
         return len(self.appointments) - 1
@@ -56,25 +64,27 @@ class veterinary_practice:
             return appointment.get_notes()
         else:
             return "Unrecognized appointment ID"
-     
 
     def stock_medication(self, medication_name, amount):
-        """ Find the medication by the given name or, if it does not exist, creates the medication.
+        """
+        Find the medication by the given name or,
+        if it does not exist, creates the medication.
             Updates the amount of mediation that is in stock.
-            
-        :param medication_name: The name of the medication to be created/updated.
+
+        :param medication_name:
+        The name of the medication to be created/updated.
         :param amount: The amount to increase the stock by.
         """
         medication = self.find_medication(medication_name)
-        if medication: # medication exists
+        if medication:  # medication exists
             medication.restock(amount)
-        else: # medication does not exist
+        else:  # medication does not exist
             medication = Medication(medication_name, amount)
             self.medications.append(medication)
 
     def create_prescription(self, pet, medication, dosage):
         """ Create a prescription for a pet.
-        
+
         :param pet (Pet): The pet to create the prescription for
         :param medication (Medication): The medication the prescription is for
         :param dosage (int): The amount of medication to be given
@@ -84,26 +94,29 @@ class veterinary_practice:
         prescription = pet.create_prescription(medication, dosage)
         self.prescriptions.append(prescription)
 
-        return len(self.prescriptions) - 1    
-        
+        return len(self.prescriptions) - 1
+
     def prepare_prescription_for_collection(self, prescription_id):
-        """ Attempts to change the prescription status from being prepared to ready for collection
-        
+        """
+        Attempts to change the prescription status
+        from being prepared to ready for collection
+
         :param prescription_id: The ID of the prescription to be prepared
         :returns message (string)
         """
         prescription = self.find_prescription(prescription_id)
         if prescription:
-            if prescription.prepareForCollection():     
+            if prescription.prepareForCollection():
                 return "Prescription prepared"
             return "Prescription is not ready for preparation"
         else:
             return "Unrecognized prescription ID"
-        
-    
+
     def collect_prescription(self, prescription_id):
-        """ Attempts to change the prescription status from ready for collection to collected.
-        
+        """
+        Attempts to change the prescription status
+        from ready for collection to collected.
+
         :param prescription_id: The ID of the prescription to be collected
         :returns message (string)
         """
@@ -114,36 +127,35 @@ class veterinary_practice:
             return "Prescription is not ready for collection"
         else:
             return "Unrecognized prescription ID"
-    
 
-    #===================================
+    # ===================================
     # Basic access methods
 
     def has_owners(self):
         return len(self.owners) > 0
-    
+
     def has_medications(self):
         return len(self.medications) > 0
-    
+
     def has_prescriptions(self):
         return len(self.prescriptions) > 0
 
     def find_owner(self, name):
         return utils.find_by_name(name, self.owners)
-        
+
     def find_medication(self, name):
-        return utils.find_by_name(name, self.medications) 
-    
+        return utils.find_by_name(name, self.medications)
+
     def find_prescription(self, id):
         if id < len(self.prescriptions):
             return self.prescriptions[id]
         return None
-    
+
     def find_appointment(self, id):
         if id < len(self.appointments):
             return self.appointments[id]
         return None
-    
 
-# EOF    
-#------
+
+# EOF
+# ------
