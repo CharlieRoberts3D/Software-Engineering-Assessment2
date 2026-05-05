6b5fc91a71c848824bc18692082707ed27260e07
CharlieRoberts3D <30162653@students.lincoln.ac.uk>
2026-05-05 10:49:59 +0100

    Update observer.py

--- CMP1139-assessment2-starter-code/observer.py
+++ CMP1139-assessment2-starter-code/observer.py
@@ -1,3 +1,42 @@
 import medication 
 import prescription
 from abc import ABC, abstractmethod
+
+
+class Subject(ABC):
+    @abstractmethod
+    def attach(self, observer):
+        pass
+
+    @abstractmethod
+    def detach(self, observer):
+        pass
+
+    @abstractmethod
+    def notify(self):
+        pass
+
+
+class DataTable(Subject):
+    """ The concrete subject """
+    def __init__(self):
+        self._observers = []
+        self._values = []
+
+    def attach(self, observer):
+        self._observers.append(observer)
+
+    def detach(self, observer):
+        self._observers.remove(observer)
+
+    def notify(self):
+        for observer in self._observers:
+            observer.update()
+
+    def set_values(self, values):
+        """Update the data and notify observers."""
+        self._values = values
+        self.notify()
+
+    def get_values(self):
+        return self._values
