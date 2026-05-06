e431378772157c14ea62be765adfd495f81e2c6f
CharlieRoberts3D <30162653@students.lincoln.ac.uk>
2026-05-06 12:07:52 +0100

    Update appointment.py
    missed a space

--- CMP1139-assessment2-starter-code/appointment.py
+++ CMP1139-assessment2-starter-code/appointment.py
@@ -52,7 +52,7 @@ class SurgeryDecorator(AppointmentDecorator):
         return self.decorated_appointment.get_pet()
 
     def get_notes(self):
-        print("Please state any notes from the surgery that was performedif there was one, if none leave blank: ")
+        print("Please state any notes from the surgery that was performed if there was one, if none leave blank: ")
         note = input()
         if (note != ""):
             return self.decorated_appointment.get_notes() + (f", surgery notes = {note}")
